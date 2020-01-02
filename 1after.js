d86707f24bf6ac038d5b8fcdd3885e6041aa611c
Zehavit <zehavito999@gmail.com>
2020-01-02 21:29:18 +0200

    bla bla

--- 1after.js
+++ 1after.js
@@ -49,8 +49,5 @@ app.get('/ContactUs', function(req, res) {
     res.sendFile(path.join(__dirname + '/ContactUs.html'));
 });
 
-
-let server = app.listen(process.env.port || 8081, function(){
-    let port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
+const port = process.env.PORT || 8081;
+app.listen(port,() => console.log("Server started at http://localhost:%s", port))
